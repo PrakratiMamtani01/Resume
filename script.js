9f4aaba60ebbc94cfe38a7bf4d00aae6ed1b5972
Prakrati <71223817+PrakratiMamtani@users.noreply.github.com>
2023-11-03 14:25:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,97 @@
-function printmessage(){
-	var name = document.getElementById("name").value;
-	document.getElementById("message").innerHTML = "Good evening " + name + ", my name is Prakrati Mamtani, I'm currently completing my undergraduate studies from New York University Abu Dhabi. To view the full profile click on the arrow bellow.";
-	var line = document.createElement("br");
-	document.getElementById("message").appendChild(line);
-	var line = document.createElement("br");
-	document.getElementById("message").appendChild(line);
-	a = document.createElement("a");
-	a.href = "resume.html"
-	nextPage = document.createElement("button");
-	nextPage.id = "page_id"
-	nextPage.innerHTML = "-->";
-	a.appendChild(nextPage);
-	document.getElementById("message").appendChild(a);
+// Function to handle modal
+function handleModal(openBtn, modal, closeBtn) {
+    openBtn.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
 }
 
+// Handle modals
+handleModal(
+    document.getElementById("CoursesBtn"),
+    document.getElementById("CoursesModal"),
+    document.getElementById("CloseCourses")
+);
+
+handleModal(
+    document.getElementById("SkillsBtn"),
+    document.getElementById("SkillsModal"),
+    document.getElementById("CloseSkills")
+);
+
+handleModal(
+    document.getElementById("ContactBtn"),
+    document.getElementById("ContactModal"),
+    document.getElementById("CloseContact")
+);
+
+
+function setupFlashcardModal(openBtnId, modalId, flashcards_class, nextBtnId, closeBtnId) {
+    const openBtn = document.getElementById(openBtnId);
+    const modal = document.getElementById(modalId);
+    const flashcards = modal.querySelectorAll(flashcards_class);
+    const closeBtn = document.getElementById(closeBtnId);
+
+    let currentFlashcardIndex = 0;
+
+    function showFlashcard(index) {
+        flashcards.forEach((flashcard, i) => {
+            flashcard.style.display = i === index ? "block" : "none";
+        });
+    }
+
+    openBtn.addEventListener("click", function () {
+        modal.style.display = "block";
+        currentFlashcardIndex = 0;
+        showFlashcard(currentFlashcardIndex);
+    });
+
+    const nextBtn = document.getElementById(nextBtnId);
+
+    nextBtn.addEventListener("click", function () {
+        console.log("Here")
+        // Hide the current flashcard
+        flashcards[currentFlashcardIndex].style.display = "none";
+
+        // Move to the next flashcard
+        currentFlashcardIndex++;
+
+        // If we reach the end, loop back to the first flashcard
+        if (currentFlashcardIndex >= flashcards.length) {
+            currentFlashcardIndex = 0;
+        }
+
+        // Show the next flashcard
+        flashcards[currentFlashcardIndex].style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+        flashcards.forEach(flashcard => (flashcard.style.display = "none"));
+    });
+}
+
+
+// Set up the Work Flashcards
+setupFlashcardModal("WorkBtn", "flashcardsWorkModal", ".work", "nextWorkFlashcard", "closeWorkFlashcards");
+
+// Set up the Leadership Flashcards
+setupFlashcardModal("LeadBtn", "flashcardsLeadModal", ".leadership", "nextLeadFlashcard", "closeLeadFlashcards");
+
+
+
+// Changing greeting
 const greeting = Array('Bonjour!!', 'Hola!!', 'Ciao!!', 'Welcome!!', 'Marhaba!!', "Hello!!");
 const greet = document.getElementById("greet");
 greet.innerHTML = "Hello!!";
 idx = 0;
 
-setInterval(function() {
-	greet.innerHTML = greeting[idx];
-	idx += 1;
-	idx = idx % greeting.length;
 
+setInterval(function() {
+    greet.innerHTML = greeting[idx];
+    idx += 1;
+    idx = idx % greeting.length;
 }, 1000);
